235b61c8a4ca46709eb6adde808e0c7fb4a1525b
Sarthak-code360 <sarthakmishra3032002@gmail.com>
2026-04-11 18:32:27 +0530

    bg black for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   const [contactOpen, setContactOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-black">
       <div className="max-w-[1400px] mx-auto px-6 lg:px-12 flex items-center justify-between h-16 lg:h-20">
         <img
           src={ComLogo1}
